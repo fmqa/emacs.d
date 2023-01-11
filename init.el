3738d57b4a64077c6e102e52e6527ebfa65bcbe2
F.S. Moukayed <7354509+fmqa@users.noreply.github.com>
2023-01-11 19:48:15 +0100

    Add whitespace settings

--- init.el
+++ init.el
@@ -34,16 +34,19 @@
  '(desktop-save-mode t)
  '(evil-mode t)
  '(evil-undo-system 'undo-redo)
+ '(global-whitespace-mode t)
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
  '(package-selected-packages '(which-key evil dracula-theme))
  '(tab-width 4)
  '(tool-bar-mode nil)
  '(undo-no-redo t)
- '(which-key-mode t))
+ '(which-key-mode t)
+ '(whitespace-style
+   '(face trailing tabs spaces lines newline missing-newline-at-eof empty space-after-tab space-before-tab tab-mark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(whitespace-indentation ((t nil))))
