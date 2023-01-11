b15fbd98b99c32173a72e803d0ce773543486a74
F.S. Moukayed <7354509+fmqa@users.noreply.github.com>
2023-01-11 18:23:19 +0100

    Create init.el

---
+++ init.el
@@ -0,0 +1,39 @@
+;; MELPA
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; Bind Ctrl+Menu to open the global menu 
+(global-set-key (kbd "C-<menu>") 'menu-bar-open)
+
+(if (fboundp 'use-package)
+    (use-package evil))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cua-mode t)
+ '(custom-enabled-themes '(dracula))
+ '(custom-safe-themes
+   '("05626f77b0c8c197c7e4a31d9783c4ec6e351d9624aa28bc15e7f6d6a6ebd926" default))
+ '(desktop-save-mode t)
+ '(evil-mode t)
+ '(evil-undo-system 'undo-redo)
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(package-selected-packages '(use-package which-key dracula-theme evil))
+ '(tab-width 4)
+ '(tool-bar-mode nil)
+ '(undo-no-redo t)
+ '(which-key-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
