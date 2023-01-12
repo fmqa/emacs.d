1cf958e6b2624218fe6cac53c4fe828ae30651af
F.S. Moukayed <7354509+fmqa@users.noreply.github.com>
2023-01-12 19:51:27 +0100

    Update init.el

--- init.el
+++ init.el
@@ -22,6 +22,10 @@
   (package-refresh-contents)
   (package-install 'dracula-theme))
 
+;; Notes:
+;;
+;; * `rectangle-preview' is disabled due to an overlay clearing bug
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
