86cc8d71a524c9a6e44b8540db9f606b1ad33ff3
F.S. Moukayed <7354509+fmqa@users.noreply.github.com>
2023-02-10 12:08:40 +0100

    Use less jarring background for indent markers

--- init.el
+++ init.el
@@ -109,4 +109,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(whitespace-indentation ((t nil))))
+ '(whitespace-indentation ((t nil)))
+ '(whitespace-tab ((t (:background "#282a36" :foreground "#3d4766")))))
