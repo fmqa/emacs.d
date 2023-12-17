a453d9b02ccd84d91ec7966a33f97a0a6112afdb
Ф.З. Мкауед <7354509+fmqa@users.noreply.github.com>
2023-12-18 00:29:22 +0100

    Enable recursive minibuffers

--- init.el
+++ init.el
@@ -63,6 +63,7 @@
 								(side . left))
 	 (erc-mode-p display-buffer-reuse-mode-window
 				 (mode erc-mode))))
+ '(enable-recursive-minibuffers t)
  '(erc-fill-column 158)
  '(erc-interpret-mirc-color t)
  '(erc-modules
@@ -77,6 +78,7 @@
  '(initial-scratch-message nil)
  '(isearch-lazy-count t)
  '(menu-bar-mode nil)
+ '(minibuffer-depth-indicate-mode t)
  '(org-replace-disputed-keys t)
  '(pixel-scroll-precision-mode t)
  '(prettify-symbols-unprettify-at-point 'right-edge)
@@ -100,3 +102,9 @@
  '(whitespace-style
    '(face trailing tabs newline missing-newline-at-eof empty space-after-tab space-before-tab tab-mark)))
 (put 'narrow-to-region 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
